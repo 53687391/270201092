933608577fa97afcb8935833821d8fd7f75d3be2
53687391 <burakcil@std.iyte.edu.tr>
2020-10-19 09:18:50 +0000

    root finding

--- lab1/example1.py
+++ lab1/example1.py
@@ -5,9 +5,21 @@ chickens = 6
 roosters = animals-chickens
 print("There are ",roosters,"roosters.")
 """
+
+"""
 x = 1
 y = 4
 z = 0.25
 int(x),int(y),int(z)
 mass_equation = ((((2*x)+y)**2)*(z**0.5))/((x**0.5)+(y**0.5))
 print(mass_equation)
+"""
+
+a =2
+b = 6
+c = -20
+int(a), int(b), int(c)
+d = (b**2) - (4*a*c)
+x1 = (-b-d**0.5)/(2*a)
+x2 = (-b+d**0.5)/(2*a)
+print("The roots are ",x1,"and",x2)
