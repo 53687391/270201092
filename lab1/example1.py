7aacd53b6ffe9eb6f92710a87355087ced172455
53687391 <burakcil@std.iyte.edu.tr>
2020-10-19 09:06:26 +0000

    mass equation

--- lab1/example1.py
+++ lab1/example1.py
@@ -1,5 +1,13 @@
 #print("Hello World!")
+"""
 animals = 10
 chickens = 6
 roosters = animals-chickens
 print("There are ",roosters,"roosters.")
+"""
+x = 1
+y = 4
+z = 0.25
+int(x),int(y),int(z)
+mass_equation = ((((2*x)+y)**2)*(z**0.5))/((x**0.5)+(y**0.5))
+print(mass_equation)
