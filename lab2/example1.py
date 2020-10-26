1c355f9d6e62ab2321850935a407ffc581d95a35
53687391 <burakcil@std.iyte.edu.tr>
2020-10-26 08:44:03 +0000

    Minimum Value

--- lab2/example1.py
+++ lab2/example1.py
@@ -1,10 +1,23 @@
 #Absolute Value
-number = int(input("What is the number? "))
+
+"""
+number = float(input("What is the number? \n"))
 if number < 0:
   number = number*-1
-  print(number)
+  print("Absolute value: ",number)
 else:
-  number=number
-  print(number)
+  print("Absolute value: ",number)
+
+"""
+
+# Minimum Value
+
+number_1 = float(input("What is the number1 ?\n"))
+number_2 = float(input("What is the number2 ?\n"))
+number_3 = float(input("What is the number3 ?\n"))
+
+print("Minimum value = ",min(number_1,number_2,number_3))
+
+
 
 
